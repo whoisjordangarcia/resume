6c2531084c5e6ac3b0daa0e9bc42dfacf16670de
Jordan Garcia <arickho@gmail.com>
2024-03-09 09:12:34 -0500

    fix syntax error

--- src/data/individualContributor.ts
+++ src/data/individualContributor.ts
@@ -1,52 +1,53 @@
 const individualContributor = {
-    jobTitle: 'Engineering Lead',
-    profileContent: "A seasoned Software Engineer with 10+ years of hands-on experience in developing robust web applications and backend services. Expert in creating complex solutions and adept in web development. Proven track record of building strong teams, demonstrating leadership, delivering project milestones, and developing strategic roadmaps. Proficient in fostering team growth, encouraging cross-functional collaboration, implementing agile methodologies, and mentoring.",
-    jobs: [
-        {
-            position: "Engineering Lead",
-            company: "Invitae",
-            location: "Remote",
-            jobDuration: "Nov 2018 - Present",
-            hightlights: [
-                "Led the identity team, migrating 4.5 million user accounts to Auth0 with zero downtime.",
-                "Led the development of an centralized AuthZ (Authorization) system enhancing resource sharing capabilities and auditing.",
-                "Managed a diverse team comprising from junior to principal individual contributors and contractors, maintaining high productivity and team collaboration.",
-                "Improved teams CI/CD best practices and overall system availability significantly",
-                "Developed a tool to assist teams with CI/CD pipelines in testing web features pre-merge, enabling stakeholders to evaluate new UI features. Adopted across the engineering organization.",
-                "Migrated invitae.com from a legacy Django CMS to Contentful, building a reusable CMS UI library. This allowed marketing to publish new pages on demand versus weekly/monthly releases",
-                "Led the decentralization of the largest monolithic application into microservices, elevating system reliability, maintainability, and resilience."
-        },
-        {
-            position: "Senior Software Engineer",
-            company: "TodayTix",
-            location: "New York, NY",
-            jobDuration: "Oct 2017 - Oct 2018",
-            hightlights: [
-                "Launched a high impact broadway show lottery feature, boosting site traffic by 20%.",
-                "Migrated todaytix.com from javascript to typescript, enhancing code reliability and maintainability."
-            ]
-        },
-        {
-            position: "Senior Software Engineer",
-            company: "Tigerspike",
-            location: "New York, NY",
-            jobDuration: "Feb 2014 - Oct 2017",
-            hightlights: [
-                "Redesigned a global intranet for a leading financial firm, integrating efficient search capabilities for the user directory.",
-                "Re-platformed an asset management website, supporting over 7 different locales."
-            ]
-        },
-        {
-            position: "Software Engineer",
-            company: "Simbiotic",
-            location: "Perth, Western Australia",
-            jobDuration: "Jul 2011 - Jan 2014",
-            hightlights: [
-                "Developed an algorithm to generate time-based cycle forecasting to notify rail operation engineers of workflow points affecting cycle times.",
-                "Created a visual map that helped identify all locomotives' exact locations in real-time."
-            ]
-        },
-    ],
+  jobTitle: 'Engineering Lead',
+  profileContent: "A seasoned Software Engineer with 10+ years of hands-on experience in developing robust web applications and backend services. Expert in creating complex solutions and adept in web development. Proven track record of building strong teams, demonstrating leadership, delivering project milestones, and developing strategic roadmaps. Proficient in fostering team growth, encouraging cross-functional collaboration, implementing agile methodologies, and mentoring.",
+  jobs: [
+    {
+      position: "Engineering Lead",
+      company: "Invitae",
+      location: "Remote",
+      jobDuration: "Nov 2018 - Present",
+      hightlights: [
+        "Led the identity team, migrating 4.5 million user accounts to Auth0 with zero downtime.",
+        "Led the development of an centralized AuthZ (Authorization) system enhancing resource sharing capabilities and auditing.",
+        "Managed a diverse team comprising from junior to principal individual contributors and contractors, maintaining high productivity and team collaboration.",
+        "Improved teams CI/CD best practices and overall system availability significantly",
+        "Developed a tool to assist teams with CI/CD pipelines in testing web features pre-merge, enabling stakeholders to evaluate new UI features. Adopted across the engineering organization.",
+        "Migrated invitae.com from a legacy Django CMS to Contentful, building a reusable CMS UI library. This allowed marketing to publish new pages on demand versus weekly/monthly releases",
+        "Led the decentralization of the largest monolithic application into microservices, elevating system reliability, maintainability, and resilience.",
+      ],
+    },
+    {
+      position: "Senior Software Engineer",
+      company: "TodayTix",
+      location: "New York, NY",
+      jobDuration: "Oct 2017 - Oct 2018",
+      hightlights: [
+        "Launched a high impact broadway show lottery feature, boosting site traffic by 20%.",
+        "Migrated todaytix.com from javascript to typescript, enhancing code reliability and maintainability."
+      ]
+    },
+    {
+      position: "Senior Software Engineer",
+      company: "Tigerspike",
+      location: "New York, NY",
+      jobDuration: "Feb 2014 - Oct 2017",
+      hightlights: [
+        "Redesigned a global intranet for a leading financial firm, integrating efficient search capabilities for the user directory.",
+        "Re-platformed an asset management website, supporting over 7 different locales."
+      ]
+    },
+    {
+      position: "Software Engineer",
+      company: "Simbiotic",
+      location: "Perth, Western Australia",
+      jobDuration: "Jul 2011 - Jan 2014",
+      hightlights: [
+        "Developed an algorithm to generate time-based cycle forecasting to notify rail operation engineers of workflow points affecting cycle times.",
+        "Created a visual map that helped identify all locomotives' exact locations in real-time."
+      ]
+    },
+  ],
 }
 
 export { individualContributor }
