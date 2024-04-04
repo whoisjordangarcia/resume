bbcae1a91092863359eadda8bf2c2dcac9c9f039
Jordan Garcia <arickho@gmail.com>
2024-04-03 20:38:57 -0400

    adds initial for for resume builder

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -1,18 +1,103 @@
-import Link from "next/link";
-
 import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
+import { resumeContent } from "~/app/data/resume-content";
 
 export default async function ResumeBuilder() {
-
+  const content = resumeContent;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem] text-white">
           Resume Builder
         </h1>
-        
-        <CrudShowcase />
+        <form>
+          <div className="space-y-12 border-gray-900">
+            <div className="border-b border-gray-900/10 bg-white pb-20 p-6 rounded-md">
+              <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-8">
+                <div className="sm:col-span-4">
+                  <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Name
+                  </label>
+                  <div className="mt-2">
+                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                      <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        disabled
+                        value={content.name}
+                        className="block flex-1 border-0 bg-gray py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-s"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="sm:col-span-4">
+                  <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Job Title
+                  </label>
+                  <div className="mt-2">
+                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                      <input
+                        type="text"
+                        name="jobTitle"
+                        id="jobTitle"
+                        disabled
+                        value={content.jobTitle}
+                        className="block flex-1 border-0 bg-gray py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-s"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="sm:col-span-4">
+                  <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Location
+                  </label>
+                  <div className="mt-2">
+                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                      <input
+                        type="text"
+                        name="location"
+                        id="location"
+                        disabled
+                        value={content.location}
+                        className="block flex-1 border-0 bg-gray py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-s"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="sm:col-span-4">
+                  <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Email
+                  </label>
+                  <div className="mt-2">
+                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                      <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        disabled
+                        value={content.email}
+                        className="block flex-1 border-0 bg-gray py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-s"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6 flex items-center justify-end gap-x-6">
+            <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Save
+            </button>
+          </div>
+        </form>
       </div>
     </main>
   );
@@ -22,7 +107,7 @@ async function CrudShowcase() {
   const latestPost = await api.post.getLatest();
 
   return (
-    <div className="w-full max-w-xs">
+    <div className="w-full max-w-xs text-white">
       {latestPost ? (
         <p className="truncate">Your most recent post: {latestPost.name}</p>
       ) : (
