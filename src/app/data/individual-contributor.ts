071546491822a24ba2a1714ca2298b7bb750e28f
Jordan Garcia <arickho@gmail.com>
2024-04-23 21:03:21 -0400

    fix grammar issues

--- src/app/data/individual-contributor.ts
+++ src/app/data/individual-contributor.ts
@@ -21,8 +21,8 @@ const individualContributor = {
           location: "Remote, FL",
           highlights: [
             "Managed team of 11 direct reports, customer experiences for both patients and physicians portals, fostering high productivity and cross-team collaboration.",
-            "Worked closely with Product on driving the teams strategic roadmap, helping to lead the team in exceeding goals and working towards the companies north-star.",
-            "Collaborated with product and lead the team on changing ordering workflow to insurance re-imbursements rejections and being able to successfully collect $5M+ per quarter.",
+            "Planned and prioritized with Product on driving the teams strategic roadmap, helping to lead the team in exceeding goals and working towards the companies north-star.",
+            "Collaborated with product and lead the team on changing ordering workflow to insurance reimbursements rejections and being able to collect $5M+ per quarter.",
           ],
         },
         {
@@ -30,8 +30,8 @@ const individualContributor = {
           jobDuration: "November 2018 - May 2021",
           location: "New York, NY",
           highlights: [
-            "Build a service that generated genetic reports for 4M+ patients, creating a comprehensive testing suite, with 98.6% test coverage, including visual snapshot testing.",
-            "De-coupled a large monolithic application into micoservices, reducing deployments from 2-3 weeks down to weekly daily releases, and fixing overprovisioned resources saving $25k per month",
+            "Built a service that generated genetic reports for 4M+ patients, creating a comprehensive testing suite, with 98.6% test coverage, including visual snapshot testing.",
+            "Decoupled a large monolithic application into microservices, reducing deployments from 2-3 weeks down to weekly daily releases, and fixing overprovisioned resources saving $25k per month.",
           ],
         },
       ],
@@ -44,8 +44,8 @@ const individualContributor = {
           jobDuration: "October 2017 - October 2018",
           location: "New York, NY",
           highlights: [
-            "Produced web experience that allows users to pick their own seats when buying a broadway show, this resulted in increasing web revenue by 30% each quarter.",
-            "Led the technical migration web experience from JavaScript to TypeScript within 2 months, substantially reducing the amount of production issues, and improving developer experience by spotting out common bugs during static analysis tooling",
+            "Produced web experience that allows users to choose own seats when purchasing a broadway show, this resulted in increasing web revenue by 30% each quarter.",
+            "Led the technical migration web experience from JavaScript to TypeScript within 2 months, reducing the amount of production issues, and improving developer experience by spotting out common bugs during static analysis tooling.",
           ],
         },
       ],
@@ -58,14 +58,14 @@ const individualContributor = {
           jobDuration: "July 2016 - October 2017",
           location: "New York, NY",
           highlights: [
-            "Replatformed global intranet using Sharepoint 2016, improving user engagement. Used JMeter to load test and iterate and optimize on the infrastructure topology to support 5k concurrent users.",
+            "Replatformed global intranet using Microsoft Sharepoint 2016, improving user engagement. Used JMeter to load test and iterate and optimize on the infrastructure topology to support 5k concurrent users.",
             "Led and re-platformed website to support multi-languages and across 7 different regions, utilizing Tridion as the headless CMS, ASP.NET C# on the backend, Angular on the frontend, Solr for full-site search.",
-            "Recognized with MVP Award 2017 for successfully deliverying multiple projects.",
+            "Recognized with MVP Award 2017 for delivering high impact projects.",
           ],
         },
         {
           title: "Software Engineer",
-          jobDuration: "Feburary 2014 - July 2016",
+          jobDuration: "February 2014 - July 2016",
           location: "Sydney, Australia",
           highlights: [
             "Collaborated closely with the innovation team at a large bank to enhance the user experience for international money transfers, the first for the company.",
@@ -82,8 +82,8 @@ const individualContributor = {
           jobDuration: "July 2011 - January 2014",
           location: "Perth, Western Australia",
           highlights: [
-            "Created algorithm in C# on time-based cycle forecasting that empowers rail operation engineers to improve overall efficiency and client saving $1m+ a month",
-            "Full-stack developed an desktop application, deployed onto multiple touchscreen devices that tracked all locomotives within the mining site, improving logistics managment and operation response times.",
+            "Created algorithm in C# on time-based cycle forecasting that empowers rail operation engineers to improve overall efficiency and client saving $1m+ a month.",
+            "Full-stack developed an desktop application, deployed onto touchscreen devices that tracked all locomotives within the mining site, improving logistics management and operation response times.",
           ],
         },
       ],
